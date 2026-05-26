e30e85f2b450f2377da196ca81eaaf3b63605d44
Siddarth Banerjee <siddarth2801@gmail.com>
2026-05-26 10:52:43 +0530

    first commit: application.properties updated

---
+++ src/main/java/org/example/authspringbootsecuritymysql/AuthSpringbootSecurityMySqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.authspringbootsecuritymysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringbootSecurityMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSpringbootSecurityMySqlApplication.class, args);
+    }
+
+}
